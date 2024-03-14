242ccca01038e48ec8bf3cadb39c3ce7016e31fb
Em21638work <156359825+Em21638work@users.noreply.github.com>
2024-03-14 16:28:35 -0700

    First commit

--- main.hpp
+++ main.hpp
@@ -1,8 +1,55 @@
+#include <iostream>
+using namespace std;
 
-//********************
-// Declare Function Prototypes
-//********************
+void getTwoValues(int &begin, int &end);
+int getNextPrime(int begin);
+int getPervPrime(int end);
 
-// ******************************
-// Implement all your functions here
-// ******************************
+void getTwoValues(int &begin, int &end) {
+    cout << "Give two numbers " << endl;
+    cin >> begin >> end;
+
+    while (begin > end) {
+        cout << "Give two diffrent numbers " << endl;
+        cin >> begin >> end;
+    }
+}
+
+int getNextPrime(int begin) {
+    int prime;
+    for (int i = begin++; i < 0; i++) {
+        prime = 0;
+        for (int j = 2; j < 100; j++ ) {
+            if (i == j) {
+                j++;
+            }
+            if (i % j == 0) {
+                prime++;
+            }
+        }
+        if (prime == 0) {
+            cout <<" The next prime number is " << i << endl;
+            i = -2;
+        }
+    }
+}
+
+int getPervPrime(int end) {
+    int prime;
+    for (int i = end--; i < 0; i--) {
+        prime = 0;
+        for (int j = 2, j < 100; j++) {
+            if (i == j) {
+                j++;
+            }
+            if (i % j == 0){
+                prime++;
+            }
+        }
+        if (prime == 0) {
+            cout << " The pervious prime number is " << i << endl;
+            i = -2; 
+        }
+    }
+}
+
